4abd77ab87c3cca97e07ac551fef13cc42c69750
Aidan Feldman <aidan.feldman@gmail.com>
2011-08-20 23:49:28 -0400

    draw text

--- javascripts/graffiti.js
+++ javascripts/graffiti.js
@@ -16,12 +16,24 @@ var Graffiti = {
   
   paper: undefined,
   backgroundImage: undefined,
+  sourceText: 'Graffiti.js',
+  textObj: undefined,
   
   init: function(){
     this.paper = Raphael(0, 0, this.WIDTH, this.HEIGHT);
     
     var background = this.SURFACE_PHOTOS[0];
     this.backgroundImage = this.paper.image(background.src, 0, 0, this.paper.width, this.paper.height);
+     
+    this.redrawText();
+  },
+  
+  redrawText: function(){
+    var font = this.paper.getFont("Amsterdam Graffiti", 800),
+      textObj = this.paper.print(50, this.paper.height/2, this.sourceText, font, 200);
+    
+    textObj.attr({fill: 'white'});
+    this.textObj = textObj;
   }
 };
 
